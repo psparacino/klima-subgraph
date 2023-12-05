0318c30a7d0a8fa77e2bb977660868dd49d05370
psparacino <tufnelenterprises@gmail.com>
2023-12-05 13:37:42 -0500

    mumbai pdg config

--- polygon-digital-carbon/src/RetirementHandler.ts
+++ polygon-digital-carbon/src/RetirementHandler.ts
@@ -1,7 +1,7 @@
 import { MCO2_ERC20_CONTRACT, ZERO_ADDRESS } from '../../lib/utils/Constants'
 import { ZERO_BI } from '../../lib/utils/Decimals'
-import { C3OffsetNFT, VCUOMinted } from '../generated/C3-Offset/C3OffsetNFT'
-import { CarbonOffset } from '../generated/MossCarbonOffset/CarbonChain'
+// import { C3OffsetNFT, VCUOMinted } from '../generated/C3-Offset/C3OffsetNFT'
+// import { CarbonOffset } from '../generated/MossCarbonOffset/CarbonChain'
 import { Retired, Retired1 as Retired_1_4_0 } from '../generated/templates/ToucanCarbonOffsets/ToucanCarbonOffsets'
 import { incrementAccountRetirements, loadOrCreateAccount } from './utils/Account'
 import { loadCarbonCredit, loadOrCreateCarbonCredit } from './utils/CarbonCredit'
@@ -93,90 +93,90 @@ export function saveToucanRetirement_1_4_0(event: Retired_1_4_0): void {
   )
 }
 
-export function handleVCUOMinted(event: VCUOMinted): void {
-  // Currently the NFT minting is required and happens within every offset or offsetFor transaction made against a C3T
-  // This event only emits who receives the NFT and the token ID, although the data is stored.
-  // Update associated entities using a call to retrieve the retirement details.
+// export function handleVCUOMinted(event: VCUOMinted): void {
+//   // Currently the NFT minting is required and happens within every offset or offsetFor transaction made against a C3T
+//   // This event only emits who receives the NFT and the token ID, although the data is stored.
+//   // Update associated entities using a call to retrieve the retirement details.
 
-  let retireContract = C3OffsetNFT.bind(event.address)
+//   let retireContract = C3OffsetNFT.bind(event.address)
 
-  let projectAddress = retireContract.list(event.params.tokenId).getProjectAddress()
-  let retireAmount = retireContract.list(event.params.tokenId).getAmount()
+//   let projectAddress = retireContract.list(event.params.tokenId).getProjectAddress()
+//   let retireAmount = retireContract.list(event.params.tokenId).getAmount()
 
-  let credit = loadCarbonCredit(projectAddress)
+//   let credit = loadCarbonCredit(projectAddress)
 
-  credit.retired = credit.retired.plus(retireAmount)
-  credit.save()
+//   credit.retired = credit.retired.plus(retireAmount)
+//   credit.save()
 
-  // Ensure account entities are created for all addresses
-  loadOrCreateAccount(event.params.sender)
-  let sender = loadOrCreateAccount(event.transaction.from)
+//   // Ensure account entities are created for all addresses
+//   loadOrCreateAccount(event.params.sender)
+//   let sender = loadOrCreateAccount(event.transaction.from)
 
-  saveRetire(
-    event.transaction.from.concatI32(sender.totalRetirements),
-    projectAddress,
-    ZERO_ADDRESS,
-    'OTHER',
-    retireAmount,
-    event.params.sender,
-    '',
-    event.transaction.from,
-    '',
-    event.block.timestamp,
-    null
-  )
+//   saveRetire(
+//     event.transaction.from.concatI32(sender.totalRetirements),
+//     projectAddress,
+//     ZERO_ADDRESS,
+//     'OTHER',
+//     retireAmount,
+//     event.params.sender,
+//     '',
+//     event.transaction.from,
+//     '',
+//     event.block.timestamp,
+//     null
+//   )
 
-  recordProvenance(
-    event.transaction.hash,
-    projectAddress,
-    event.params.sender,
-    ZERO_ADDRESS,
-    'RETIREMENT',
-    retireAmount,
-    event.block.timestamp
-  )
+//   recordProvenance(
+//     event.transaction.hash,
+//     projectAddress,
+//     event.params.sender,
+//     ZERO_ADDRESS,
+//     'RETIREMENT',
+//     retireAmount,
+//     event.block.timestamp
+//   )
 
-  incrementAccountRetirements(event.transaction.from)
-}
+//   incrementAccountRetirements(event.transaction.from)
+// }
 
-export function handleMossRetirement(event: CarbonOffset): void {
-  // Don't process zero amount events
-  if (event.params.carbonTon == ZERO_BI) return
+// export function handleMossRetirement(event: CarbonOffset): void {
+//   // Don't process zero amount events
+//   if (event.params.carbonTon == ZERO_BI) return
 
-  let credit = loadOrCreateCarbonCredit(MCO2_ERC20_CONTRACT, 'MOSS')
+//   let credit = loadOrCreateCarbonCredit(MCO2_ERC20_CONTRACT, 'MOSS')
 
-  // Set up project/default info for Moss "project"
+//   // Set up project/default info for Moss "project"
 
-  if (credit.vintage == 1970) {
-    credit.vintage = 2021
-    credit.project = 'Moss'
-    credit.save()
+//   if (credit.vintage == 1970) {
+//     credit.vintage = 2021
+//     credit.project = 'Moss'
+//     credit.save()
 
-    loadOrCreateCarbonProject('VERRA', 'Moss')
-  }
+//     loadOrCreateCarbonProject('VERRA', 'Moss')
+//   }
 
-  credit.retired = credit.retired.plus(event.params.carbonTon)
-  credit.save()
+//   credit.retired = credit.retired.plus(event.params.carbonTon)
+//   credit.save()
 
-  // Ensure account entities are created for all addresses
-  loadOrCreateAccount(event.params.sender)
-  let sender = loadOrCreateAccount(event.transaction.from)
+//   // Ensure account entities are created for all addresses
+//   loadOrCreateAccount(event.params.sender)
+//   let sender = loadOrCreateAccount(event.transaction.from)
 
-  saveRetire(
-    event.transaction.from.concatI32(sender.totalRetirements),
-    MCO2_ERC20_CONTRACT,
-    MCO2_ERC20_CONTRACT,
-    'OTHER',
-    event.params.carbonTon,
-    event.params.sender,
-    event.params.onBehalfOf,
-    event.transaction.from,
-    '',
-    event.block.timestamp,
-    null
-  )
+//   saveRetire(
+//     event.transaction.from.concatI32(sender.totalRetirements),
+//     MCO2_ERC20_CONTRACT,
+//     MCO2_ERC20_CONTRACT,
+//     'OTHER',
+//     event.params.carbonTon,
+//     event.params.sender,
+//     event.params.onBehalfOf,
+//     event.transaction.from,
+//     '',
+//     event.block.timestamp,
+//     null
+//   )
 
-  incrementAccountRetirements(event.transaction.from)
-}
+//   incrementAccountRetirements(event.transaction.from)
+// }
 
-// export function handleMossRetirementToMainnet(): void {}
+// // export function handleMossRetirementToMainnet(): void {}
--- polygon-digital-carbon/src/TransferHandler.ts
+++ polygon-digital-carbon/src/TransferHandler.ts
@@ -1,6 +1,6 @@
 import { store } from '@graphprotocol/graph-ts'
 import { MCO2_ERC20_CONTRACT, TOUCAN_CROSS_CHAIN_MESSENGER, ZERO_ADDRESS } from '../../lib/utils/Constants'
-import { Transfer } from '../generated/BCT/ERC20'
+// import { Transfer } from '../generated/BCT/ERC20'
 import { loadCarbonCredit, loadOrCreateCarbonCredit } from './utils/CarbonCredit'
 import { Retired, Retired1 as Retired_1_4_0 } from '../generated/templates/ToucanCarbonOffsets/ToucanCarbonOffsets'
 import { loadOrCreateHolding } from './utils/Holding'
@@ -14,140 +14,140 @@ import { checkForCarbonPoolCreditSnapshot } from './utils/CarbonPoolCreditBalanc
 import { loadOrCreateEcosystem } from './utils/Ecosystem'
 import { recordProvenance } from './utils/Provenance'
 
-export function handleCreditTransfer(event: Transfer): void {
-  // Ignore transfers of zero value
-  if (event.params.value == ZERO_BI) return
+// export function handleCreditTransfer(event: Transfer): void {
+//   // Ignore transfers of zero value
+//   if (event.params.value == ZERO_BI) return
 
-  if (event.address == MCO2_ERC20_CONTRACT) loadOrCreateCarbonCredit(MCO2_ERC20_CONTRACT, 'MOSS')
-  let credit = loadCarbonCredit(event.address)
+//   if (event.address == MCO2_ERC20_CONTRACT) loadOrCreateCarbonCredit(MCO2_ERC20_CONTRACT, 'MOSS')
+//   let credit = loadCarbonCredit(event.address)
 
-  if (event.params.from == ZERO_ADDRESS) {
-    credit.currentSupply = credit.currentSupply.plus(event.params.value)
+//   if (event.params.from == ZERO_ADDRESS) {
+//     credit.currentSupply = credit.currentSupply.plus(event.params.value)
 
-    // Do not flag cross chain bridging as an actual bridge event
-    let crossChain = CrossChainBridge.load(event.transaction.hash.concatI32(event.transactionLogIndex.toI32() - 1))
-    if (crossChain == null) {
-      credit.bridged = credit.bridged.plus(event.params.value)
+//     // Do not flag cross chain bridging as an actual bridge event
+//     let crossChain = CrossChainBridge.load(event.transaction.hash.concatI32(event.transactionLogIndex.toI32() - 1))
+//     if (crossChain == null) {
+//       credit.bridged = credit.bridged.plus(event.params.value)
 
-      saveBridge(
-        event.transaction.hash,
-        event.transactionLogIndex.toI32(),
-        event.address,
-        event.params.to,
-        event.params.value,
-        event.block.timestamp
-      )
+//       saveBridge(
+//         event.transaction.hash,
+//         event.transactionLogIndex.toI32(),
+//         event.address,
+//         event.params.to,
+//         event.params.value,
+//         event.block.timestamp
+//       )
 
-      if (event.address != MCO2_ERC20_CONTRACT) {
-        recordProvenance(
-          event.transaction.hash,
-          event.address,
-          event.params.from,
-          event.params.to,
-          'ORIGINATION',
-          event.params.value,
-          event.block.timestamp
-        )
+//       if (event.address != MCO2_ERC20_CONTRACT) {
+//         recordProvenance(
+//           event.transaction.hash,
+//           event.address,
+//           event.params.from,
+//           event.params.to,
+//           'ORIGINATION',
+//           event.params.value,
+//           event.block.timestamp
+//         )
 
-        credit.provenanceCount += 1
-      }
-    }
-  } else {
-    loadOrCreateAccount(event.params.from)
-    let fromHolding = loadOrCreateHolding(event.params.from, event.address)
-    fromHolding.amount = fromHolding.amount.minus(event.params.value)
-    fromHolding.lastUpdated = event.block.timestamp
-    fromHolding.save()
+//         credit.provenanceCount += 1
+//       }
+//     }
+//   } else {
+//     loadOrCreateAccount(event.params.from)
+//     let fromHolding = loadOrCreateHolding(event.params.from, event.address)
+//     fromHolding.amount = fromHolding.amount.minus(event.params.value)
+//     fromHolding.lastUpdated = event.block.timestamp
+//     fromHolding.save()
 
-    // Delete holding entity if there are no tokens held and this isn't needed for provenance records.
-    if (fromHolding.amount == ZERO_BI && fromHolding.historicalProvenanceRecords.length == 0) {
-      store.remove('Holding', fromHolding.id.toHexString())
-    }
-  }
+//     // Delete holding entity if there are no tokens held and this isn't needed for provenance records.
+//     if (fromHolding.amount == ZERO_BI && fromHolding.historicalProvenanceRecords.length == 0) {
+//       store.remove('Holding', fromHolding.id.toHexString())
+//     }
+//   }
 
-  if (event.params.to == ZERO_ADDRESS) {
-    credit.currentSupply = credit.currentSupply.minus(event.params.value)
-  } else {
-    loadOrCreateAccount(event.params.to)
-    let toHolding = loadOrCreateHolding(event.params.to, event.address)
-    toHolding.amount = toHolding.amount.plus(event.params.value)
-    toHolding.lastUpdated = event.block.timestamp
-    toHolding.save()
+//   if (event.params.to == ZERO_ADDRESS) {
+//     credit.currentSupply = credit.currentSupply.minus(event.params.value)
+//   } else {
+//     loadOrCreateAccount(event.params.to)
+//     let toHolding = loadOrCreateHolding(event.params.to, event.address)
+//     toHolding.amount = toHolding.amount.plus(event.params.value)
+//     toHolding.lastUpdated = event.block.timestamp
+//     toHolding.save()
 
-    if (event.params.from != ZERO_ADDRESS && event.address != MCO2_ERC20_CONTRACT) {
-      recordProvenance(
-        event.transaction.hash,
-        event.address,
-        event.params.from,
-        event.params.to,
-        'TRANSFER',
-        event.params.value,
-        event.block.timestamp
-      )
+//     if (event.params.from != ZERO_ADDRESS && event.address != MCO2_ERC20_CONTRACT) {
+//       recordProvenance(
+//         event.transaction.hash,
+//         event.address,
+//         event.params.from,
+//         event.params.to,
+//         'TRANSFER',
+//         event.params.value,
+//         event.block.timestamp
+//       )
 
-      credit.provenanceCount += 1
-    }
-  }
+//       credit.provenanceCount += 1
+//     }
+//   }
 
-  // Update active credits list
-  let ecosystem = loadOrCreateEcosystem()
-  let activeIndex = ecosystem.activeCredits.indexOf(event.address)
+//   // Update active credits list
+//   let ecosystem = loadOrCreateEcosystem()
+//   let activeIndex = ecosystem.activeCredits.indexOf(event.address)
 
-  if (credit.currentSupply.plus(credit.crossChainSupply) > ZERO_BI && activeIndex == -1) {
-    // Add to the list of active credits
-    let activeCredits = ecosystem.activeCredits
-    activeCredits.push(event.address)
-    ecosystem.activeCredits = activeCredits
-    ecosystem.save()
-  } else if (credit.currentSupply.plus(credit.crossChainSupply) == ZERO_BI && activeIndex != -1) {
-    // Remove from the list of active credits
-    let activeCredits = ecosystem.activeCredits
-    activeCredits.splice(activeIndex, 1)
-    ecosystem.activeCredits = activeCredits
-    ecosystem.save()
-  }
+//   if (credit.currentSupply.plus(credit.crossChainSupply) > ZERO_BI && activeIndex == -1) {
+//     // Add to the list of active credits
+//     let activeCredits = ecosystem.activeCredits
+//     activeCredits.push(event.address)
+//     ecosystem.activeCredits = activeCredits
+//     ecosystem.save()
+//   } else if (credit.currentSupply.plus(credit.crossChainSupply) == ZERO_BI && activeIndex != -1) {
+//     // Remove from the list of active credits
+//     let activeCredits = ecosystem.activeCredits
+//     activeCredits.splice(activeIndex, 1)
+//     ecosystem.activeCredits = activeCredits
+//     ecosystem.save()
+//   }
 
-  credit.save()
+//   credit.save()
 
-  // Also save supply changes for MCO2
-  if (event.address == MCO2_ERC20_CONTRACT && (event.params.to == ZERO_ADDRESS || event.params.from == ZERO_ADDRESS)) {
-    checkForCarbonPoolSnapshot(event.address, event.block.timestamp, event.block.number)
-    checkForCarbonPoolCreditSnapshot(event.address, event.address, event.block.timestamp, event.block.number)
+//   // Also save supply changes for MCO2
+//   if (event.address == MCO2_ERC20_CONTRACT && (event.params.to == ZERO_ADDRESS || event.params.from == ZERO_ADDRESS)) {
+//     checkForCarbonPoolSnapshot(event.address, event.block.timestamp, event.block.number)
+//     checkForCarbonPoolCreditSnapshot(event.address, event.address, event.block.timestamp, event.block.number)
 
-    let pool = loadOrCreateCarbonPool(event.address)
+//     let pool = loadOrCreateCarbonPool(event.address)
 
-    if (event.params.to == ZERO_ADDRESS) pool.supply = pool.supply.minus(event.params.value)
-    else pool.supply = pool.supply.plus(event.params.value)
+//     if (event.params.to == ZERO_ADDRESS) pool.supply = pool.supply.minus(event.params.value)
+//     else pool.supply = pool.supply.plus(event.params.value)
 
-    pool.save()
-  }
-}
+//     pool.save()
+//   }
+// }
 
-export function handlePoolTransfer(event: Transfer): void {
-  // Ignore transfers of zero value
-  if (event.params.value == ZERO_BI) return
+// export function handlePoolTransfer(event: Transfer): void {
+//   // Ignore transfers of zero value
+//   if (event.params.value == ZERO_BI) return
 
-  if (event.params.from != ZERO_ADDRESS) {
-    loadOrCreateAccount(event.params.from)
-    let fromHolding = loadOrCreateHolding(event.params.from, event.address)
-    fromHolding.amount = fromHolding.amount.minus(event.params.value)
-    fromHolding.lastUpdated = event.block.timestamp
-    fromHolding.save()
+//   if (event.params.from != ZERO_ADDRESS) {
+//     loadOrCreateAccount(event.params.from)
+//     let fromHolding = loadOrCreateHolding(event.params.from, event.address)
+//     fromHolding.amount = fromHolding.amount.minus(event.params.value)
+//     fromHolding.lastUpdated = event.block.timestamp
+//     fromHolding.save()
 
-    // Delete holding entity if there are no tokens held
-    if (fromHolding.amount == ZERO_BI) {
-      store.remove('Holding', fromHolding.id.toHexString())
-    }
-  }
+//     // Delete holding entity if there are no tokens held
+//     if (fromHolding.amount == ZERO_BI) {
+//       store.remove('Holding', fromHolding.id.toHexString())
+//     }
+//   }
 
-  if (event.params.to != ZERO_ADDRESS) {
-    loadOrCreateAccount(event.params.to)
-    let toHolding = loadOrCreateHolding(event.params.to, event.address)
-    toHolding.amount = toHolding.amount.plus(event.params.value)
-    toHolding.lastUpdated = event.block.timestamp
-    toHolding.save()
-  }
-}
+//   if (event.params.to != ZERO_ADDRESS) {
+//     loadOrCreateAccount(event.params.to)
+//     let toHolding = loadOrCreateHolding(event.params.to, event.address)
+//     toHolding.amount = toHolding.amount.plus(event.params.value)
+//     toHolding.lastUpdated = event.block.timestamp
+//     toHolding.save()
+//   }
+// }
 
 export function handleToucanRetired(event: Retired): void {
   // Ignore retirements of zero value
